7ba3ffee821213e772275ce0a6748cf76b6293bf
Lada <ladislavklejna@gmail.com>
2025-03-31 13:55:25 +0200

    neotacet obraz - portrait

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.scanapp',
   appName: 'scanapp',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    ScreenOrientation: {
+      allowOrientationChange: false,
+      preferredOrientation: 'portrait',
+    },
+  },
 };
 
 export default config;
